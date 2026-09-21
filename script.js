48a97acb8676993ff9897f59d5ce84f315366458
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-21 01:44:23 -0300

    feat: alerta de cadastro realizado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 alert("Vamos cadastrar um novo vinho! Responda às perguntas a seguir.");
 
+// coleta de dados do vinho para cadastro.
 var nomeVinho = prompt("Nome do Vinho: ");
 var tipoVinho = prompt("Tipo (Branco, Tinto, Rosé): ");
 var safraVinho = prompt("Safra (ano): ");
 var quantEstoque = prompt("Quantidade em estoque: ");
+
+alert("Cadastro realizado! Veja os detalhes no console.");
