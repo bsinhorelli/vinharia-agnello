56aec39a47d67eeaf9cf2922bdb96347abc377b1
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-21 01:40:59 -0300

    feat: implementa cadastro de vinho

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Vamos cadastrar um novo vinho! Responda às perguntas a seguir.");
+
+var nomeVinho = prompt("Nome do Vinho: ");
+var tipoVinho = prompt("Tipo (Branco, Tinto, Rosé): ");
+var safraVinho = prompt("Safra (ano): ");
+var quantEstoque = prompt("Quantidade em estoque: ");
