6a403c4306c1202df4a4472abffbc10072ec45bf
Bertola-gif <enzocbertola@gmail.com>
2026-09-21 14:38:36 -0300

    adiciona cadastro de vinho e exibe dados no console

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ var safraVinho = prompt("Safra (ano): ");
 var quantEstoque = prompt("Quantidade em estoque: ");
 
 alert("Cadastro realizado! Veja os detalhes no console.");
+
+alert("abrindo console")
+console.log
+nomeVinho
+tipoVinho
+safraVinho
+quantEstoque
